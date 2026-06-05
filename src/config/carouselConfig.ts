f55cf102587b64c3338201cb83b7fb480445edc4
Un1 2.0 <redty2011@gmail.com>
2026-06-05 20:10:42 +0200

    Modded carousel

--- src/config/carouselConfig.ts
+++ src/config/carouselConfig.ts
@@ -8,13 +8,16 @@ export interface CarouselMember {
 export const CAROUSEL_INTERVAL_MS = 4000;
 
 export const carouselMembers: CarouselMember[] = [
+  { name: 'un1verso_', image: '/skins/un1verso.png', bgColor: '#4e4e4eff' }, // Esempio: Viola
   { name: 'zZalix', image: '/skins/zZalix.png', bgColor: '#fc55f4ff' }, // Esempio: Primary Blue
-  { name: 'un1verso', image: '/skins/un1verso.png', bgColor: '#4e4e4eff' }, // Esempio: Viola
-  { name: "a\'", image: '/skins/a\'.png', bgColor: '#a2def0ff' }, // Esempio: Striking Red
   { name: 'Pirata91', image: '/skins/Pirata91.png', bgColor: '#212222ff' }, // Esempio: Blu chiaro
+  { name: "aeveloy", image: '/skins/a\'.png', bgColor: '#BD9462' },
   { name: 'gabryX2', image: '/skins/gabryX2.png', bgColor: '#36a336ff' }, // Esempio: Verde Lime
-  { name: 'MainSciamn', image: '/skins/MainSciamn.png', bgColor: '#440c0cff' }, // Esempio: Neon Cyan
   { name: 'Zeph', image: '/skins/Zeph.png', bgColor: '#5d4d2e' }, // Arancione
-  { name: 'Zlem', image: '/skins/Zlem.png', bgColor: '#dddcdcff' }, // Bianco
-  { name: 'NotAlexAgain', image: '/skins/NotAlexAgain.png', bgColor: '#7fd1b2ff' } // Esempio: Rosa
+  { name: "erRossinho", image: '/skins/erRossinho.png', bgColor: '#CFAA42' },
+  { name: "il_Muffin", image: '/skins/il_Muffin.png', bgColor: "#751C1C"},
+  { name: "BlimMC", image: '/skins/BlimMC_.png', bgColor: "#B8E9F2"},
+  { name: 'Zlem', image: '/skins/Zlem.png', bgColor: '#dddcdcff' },
+  { name: 'Antoneitorus', image: '/skins/Antoneitorus.png', bgColor: '#452A08'},
+  { name: 'SuperStepp', image: '/skins/Super_Stepp.png', bgColor: '#27A9DB'}
 ];
